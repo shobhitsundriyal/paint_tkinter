375ff945bf4bd751fc68cf57165833b4ce901032
unknown <shobhitsundriyal0170@gmail.com>
2020-07-04 21:39:23 +0530

    three brushes

--- paint.py
+++ paint.py
@@ -6,6 +6,11 @@ root.geometry('800x800')
 
 def paint(e):
     # e is event that keeps track of mouse
+    # Brush parameters
+    brush_width = 20
+    brush_color = 'green'
+    brush_type = BUTT #BUTT(SLASH), ROUND, PROJECTING(DIAMOND)
+
     #start pos
     x1 = e.x - 1
     y1 = e.y - 1
@@ -13,7 +18,7 @@ def paint(e):
     #Ending position
     x2 = e.x + 1
     y2 = e.y + 1
-    canvas.create_line(x1, y1, x2, y2, fill='red', smooth=True)
+    canvas.create_line(x1, y1, x2, y2, fill=brush_color, width=brush_width, capstyle=brush_type, smooth=True)
 
 
 
@@ -24,7 +29,9 @@ canvas.pack(pady=20)
 
 canvas.bind('<B1-Motion>', paint) # left click mouse
 
-
+#Brush options
+brush_option_frame = Frame(root)
+brush_option_frame.pack(pady=20)
 
 
 root.mainloop()
